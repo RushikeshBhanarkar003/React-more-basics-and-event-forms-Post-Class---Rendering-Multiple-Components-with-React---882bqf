06820226cf277c8da42719db9bb9ccce3f0f824e
RushikeshBhanarkar003 <93939505+RushikeshBhanarkar003@users.noreply.github.com>
2022-01-13 23:01:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h3 data-ns-test="project-name">React JS</h3>
+    <p data-ns-test="project-description">I am learning React JS</p>
+    
+    </div>
   )
 }
 
